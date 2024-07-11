0fad3d46bc1a6c7a3299ff1de7ff3d4e19e61c47
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2024-07-11 08:28:07 +0200

    scafforded basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,26 @@
+import { url } from "inspector";
 import Link from "next/link";
+
+const imageUrls = [
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/e2882937-dbb9-4d6d-8e64-c6261ce20a6f-j9jw6t.08.56.jpg",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/2232f6f7-297e-4030-be22-74f8eabbabb5-xedt2r.png",
+  "https://uploadthing-prod-sea1.s3.us-west-2.amazonaws.com/387abc6a-7b71-4fcc-8666-0bc21bf809a8-hsiicv.png",
+];
+
+const images = imageUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
+    <div className="flex flex-wrap gap-4">{
+      [...images,...images].map((image) => (
+        <div key={image.id} className="w-48">
+          <img src={image.url} alt={"Images"}/>
         </div>
-      </div>
-    </main>
+      ))
+    }
+    </div>
   );
 }
---
+++ src/components/TopNav.tsx
@@ -0,0 +1,8 @@
+export default function TopNav() {
+  return (
+  <nav className={" flex , items-center, justify-between , p-4 , font-semibold , text-xl , border-b , w-full"}>
+    <div>Gallery</div>
+    <div>Sign In</div>
+  </nav>
+  )
+}
