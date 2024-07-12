ae546fea8ede2c2c577803eed7128db364048d0a
Manfred__ <42467068+Uchindami@users.noreply.github.com>
2024-07-12 10:16:13 +0200

    added image upload function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
-import { db } from "~/server/db";
+import {db} from "~/server/db";
+import {SignedIn, SignedOut} from "@clerk/nextjs";
 
 export default async function HomePage() {
   const images = await db.query.images.findMany();
-
+  
   return (
-    <div className="flex flex-wrap gap-4">
-      {[...images, ...images].map((image) => (
+  <>
+    <SignedOut>
+      <div className={"text-xl text-center"}> Please sign up/in</div>
+    </SignedOut>
+    <SignedIn>
+      <div className="flex flex-wrap gap-4">
+        {images.map((image) => (
         <div key={image.id} className="w-48">
           <div className={"m-2"}>{image.name}</div>
-          <img src={image.url} alt={"Images"} />
+          <img src={image.url} alt={"Images"}/>
         </div>
-      ))}
-    </div>
-  );
-}
+        ))}
+      </div>
+    </SignedIn>
+  </>);}
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,6 +1,11 @@
+"use client";
+
 import {SignedIn, SignedOut, SignInButton, UserButton, UserProfile} from "@clerk/nextjs";
+import {UploadButton} from "~/utils/uploadthing";
+import {useRouter} from "next/navigation";
 
 export default function TopNav() {
+  const router = useRouter()
   return (
   <nav className={" flex , items-center, justify-between , p-4 , font-semibold , text-xl , border-b , w-full"}>
     <div>Gallery</div>
@@ -9,7 +14,23 @@ export default function TopNav() {
         <SignInButton/>
       </SignedOut>
       <SignedIn>
-        <UserButton/>
+        <div className={"flex "}>
+          <UploadButton
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            alert("Upload Completed");
+            router.refresh()
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            alert(`ERROR! ${error.message}`);
+          }}
+          />
+          <UserButton/>
+        </div>
+
       </SignedIn>
     </div>
   </nav>
